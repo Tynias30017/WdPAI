0756a65e96d9068abb42aedf8b40af6a8cfa3850
Grzegorz <grzegorz30017@gmail.com>
2026-06-05 12:18:53 +0200

    Core: Implementacja metody dispatch w Routerze i podpięcie Front Controllera

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -6,4 +6,31 @@ namespace Core;
 
 class Router
 {
+    private array $routes = [];
+
+    public function add(string $method, string $uri, array $action): void
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'uri' => $uri,
+            'action' => $action
+        ];
+    }
+
+    public function dispatch(string $uri, string $method): void
+    {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+                $class = $route['action'][0];
+                $action = $route['action'][1];
+
+                $controller = new $class();
+                $controller->$action();
+
+                return;
+            }
+        }
+
+        throw new \Exception("Route not found: $uri", 404);
+    }
 }
--- src/index.php
+++ src/index.php
@@ -1,2 +1,20 @@
 <?php
-echo "<h1>Środowisko działa! Czas na ciężary!</h1>";
+
+declare(strict_types=1);
+
+// Wymagamy pliku z klasą Router. Wkrótce dodamy autoloader, żeby nie pisać tego ręcznie.
+require_once __DIR__ . '/Core/Router.php';
+
+use Core\Router;
+
+$router = new Router();
+
+// Zmienne $_SERVER zawierają dane o żądaniu z przeglądarki
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+try {
+    $router->dispatch($uri, $method);
+} catch (Exception $e) {
+    echo "Błąd: " . $e->getMessage();
+}
